35ec1968b68d8fc51cef9644b5bf04f5fed887e7
Brent Groves <brent.groves@gmail.com>
2019-09-27 10:58:35 -0400

    first commit

--- main.py
+++ main.py
@@ -28,7 +28,8 @@ from zeep.exceptions import Fault, TransportError, XMLSyntaxError
 wsdl = "https://api.plexonline.com/DataSource/Service.asmx?WSDL"
 session = Session()
 session.auth = HTTPBasicAuth('BuscheAvillaKorsws@plex.com', '5b11b45-f59f-')
-settings = Settings(strict=False, raw_response=True)
+settings = Settings(strict=False)
+# settings = Settings(strict=False, raw_response=True)
 # history = HistoryPlugin()
 # client = Client(
 #     'http://examples.python-zeep.org/basic.wsdl',
@@ -45,135 +46,35 @@ client = Client(wsdl, transport=Transport(session=session),settings=settings)
 # try:
 
     
-request_data1 = {
-'ExecuteDataSourceRequest':{
-                'DataSourceKey':"2272",
-
-                'InputParameters':{
-                    'InputParameter':{'Name':'Workcenter_Key','Value':'61324'}
-                }
-            
+request_data = {
+    'ExecuteDataSourceRequest':{
+        'DataSourceKey':"2272",
+        'InputParameters': {
+            'InputParameter': {
+                'Name':'Workcenter_Key',
+                'Value':'61324',
+                'Required':'false',
+                'Output':'false'
+            }
         }
-        }
-request_data2 = {
-        'ExecuteDataSourceRequest':{
-            'InputParameters':{
-                'InputParameter':[{
-                    'Name':'@PLC_Name',
-                    'Value':'CNC103'
-                }]
-            },
-            'DataSourceKey':'7868'
-            
-        }
-}
-request_data3 = {
-        'ExecuteDataSourceRequest':{
-            'InputParameters':{
-                'InputParameter':{
-                    'Name':'@PLC_Name',
-                    'Value':'CNC103'
-                }
-            },
-            'DataSourceKey':'7868'
-            
-        }
-}
-request_data4 = {
-    'InputParameters':{
-        'InputParameter':{
-            'Name':'@PLC_Name',
-            'Value':'CNC103'
-        }
-    },
-    'DataSourceKey':'7868'
-}
-test2 = {
-    'DataSourceKey':"2272",
-    'InputParameters': [{
-        'InputParameter': {
-            'Name':'Workcenter_Key',
-            'Value':'61324',
-            'Required':'None',
-            'Output':'None',
-            'DefaultValue':'None',
-            'Message':'None'
-        }
-    }]
+    }
 }
 
 
 # node=client.create_message(client.service, 'ExecuteDataSource',**request_data2)
-node=client.create_message(client.service, 'ExecuteDataSource',ExecuteDataSourceRequest=test2)
+node=client.create_message(client.service, 'ExecuteDataSource',**request_data)
 tree = ET.ElementTree(node)
 # etree_to_string(tree).de.decode()
-tree.write('test.xml')
+tree.write('test2.xml')
 #     client.service.ExecuteDataSource(**request_data2)
 # except Fault as error:
 #     print(ET.tostring(error.detail))
-
-
 # print(history.last_received)
 
-request_data = {
-        'ExecuteDataSourceRequest':{
-            'InputParameters':{
-                'InputParameter':{
-                    'Name':'Workcenter_Key',
-                    'Value':61324
-                }
-            },
-            'DataSourceKey':"2272"
-            
-        }
-}
-
-
-#          <ExecuteDataSourceRequest>
-#             <DataSourceKey>7868</DataSourceKey>
-#             <InputParameters>
-#                <InputParameter>
-#                   <Value>CNC103</Value>
-#                   <Name>@PLC_Name</Name>
-#                </InputParameter>
-#             </InputParameters>
-#             <DataSourceName>Workcenter_Key_From_PLCName_Get</DataSourceName>
-#          </ExecuteDataSourceRequest>
-#       </ExecuteDataSource>
-
-                    #===========================================================
-                    # 'Name':'Workcenter_Key',
-                    # 'Value':'61324'
-                    #===========================================================
-
- 
-#         <dat:ExecuteDataSource xmlns="http://www.plexus-online.com/DataSource">
-#             <dat:ExecuteDataSourceRequest>
-#                 <dat:DataSourceKey>2272</dat:DataSourceKey>
-#                 <dat:InputParameters>
-#                     <dat:InputParameter>
-#                         <dat:Name>Workcenter_Key</dat:Name>
-#                         <dat:Value>61324</dat:Value>
-#                     </dat:InputParameter>
-#                 </dat:InputParameters>
-#             </dat:ExecuteDataSourceRequest>
-#         </dat:ExecuteDataSource>
-
-# response = client.service.ExecuteDataSource(
-#     {
-#         'ExecuteDataSourceRequest':{
-#             'DataSourceKey':'2272',
-#             'InputParameters':{
-#                 'InputParameter':{
-#                     'Name':'Workcenter_Key',
-#                     'Value':'61324'
-#                 }
-#              }   
-#         }
-#     })
-#response = client.service.ExecuteDataSource(**request_data)
-# dsn=response['DataSourceName']
-# print(dsn)
+response = client.service.ExecuteDataSource(**request_data)
+# dsn = response.get('DataSourceName')
+dsn=response['DataSourceName']
+print(dsn)
 
 
 #Here 'request_data' is the request parameter dictionary.
